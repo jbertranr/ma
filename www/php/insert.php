a6f042dc6c69c0da959c1bf5a6dbc301c69fa134
jbertranr <jbertranr@gmail.com>
2015-11-27 16:26:55 +0100

    Varis

---
+++ www/php/insert.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "mysql.hostinger.es";
+$username = "u261058157_admin";
+$password = "mImUVY2g";
+$dbname = "u261058157_ma";
+$codi = $_POST['codi'];
+$usuari_codi= $_POST['usuari_codi'];
+$adreca = $_POST['adreca'];
+$descripcio = $_POST['descripcio'];
+$imatge = $_POST['imatge'];
+$den_x = $_POST['den_x'];
+$den_y = $_POST['den_y'];
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO denuncia (codi,usuari_codi,adreca,descripcio,imatge,den_x,den_y,usu_x,usu_y) VALUES ('$codi','$usuari_codi','$adreca','$descripcio','$imatge','$den_x','$den_y','$den_x','$den_y')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?> 
---
+++ www/php/select.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "mysql.hostinger.es";
+$username = "u261058157_admin";
+$password = "mImUVY2g";
+$dbname = "u261058157_ma";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM denuncia";
+$result = $conn->query($sql);
+$rows = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+print json_encode($rows);
+$conn->close();
+?>
