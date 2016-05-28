b4e20c50edd88657b607df50bd5d91985b424cec
jbertranr <jbertranr@gmail.com>
2016-05-29 00:08:20 +0200

    Varis

--- www/php/select.php
+++ www/php/select.php
@@ -1,27 +1,28 @@
 <?php
-$servername = "mysql.hostinger.es";
-$username = "u261058157_admin";
-$password = "mImUVY2g";
-$dbname = "u261058157_ma";
+	header("Access-Control-Allow-Origin: *");
+	$servername = "mysql.hostinger.es";
+	$username = "u261058157_admin";
+	$password = "mImUVY2g";
+	$dbname = "u261058157_ma";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
 
-$sql = "(SELECT * FROM denuncia ORDER BY codi DESC LIMIT 10) ORDER BY codi ASC ";
-$result = $conn->query($sql);
-$rows = array();
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        $rows[] = $row;
-    }
-} else {
-    echo "0 results";
-}
-print json_encode($rows);
-$conn->close();
+	$sql = "(SELECT * FROM denuncia ORDER BY codi DESC LIMIT 10) ORDER BY codi ASC ";
+	$result = $conn->query($sql);
+	$rows = array();
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$rows[] = $row;
+		}
+	} else {
+		echo "0 results";
+	}
+	print json_encode($rows);
+	$conn->close();
 ?>
