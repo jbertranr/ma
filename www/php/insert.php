2958e36128ab309b4a28605007df449a1913b53f
jbertranr <jbertranr@gmail.com>
2016-05-29 20:47:00 +0200

    Status Bar

--- www/php/insert.php
+++ www/php/insert.php
@@ -28,4 +28,71 @@ if (mysqli_query($conn, $sql)) {
 
 mysqli_close($conn);
 
+// Tramesa del correu electrònic
+
+$to  = 'aidan@example.com' . ', '; // note the comma
+$to .= 'wez@example.com';
+
+// subject
+$subject = 'Birthday Reminders for August';
+
+// message
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <p>Here are the birthdays upcoming in August!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'To: Jordi <jbertranr@gmail.com>' . "\r\n";
+$headers .= 'From: Risc Animal <jbertranr@gmail.com>' . "\r\n";
+//$headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
+// $headers .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+$subject = 'Nova alerta Risc Animal';
+$message = '
+<html>
+<head>
+  <title>Nova Alerta de risc animal introduïda</title>
+</head>
+<body>
+  <p>Nova Alerta de risc animal introduïda!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// Mail it
+mail($to, $subject, $message, $headers);
+
+
 ?> 
