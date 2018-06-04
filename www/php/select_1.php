2c398e73762551fc7ef936361648799a5ae9bcf9
jbertranr <jbertranr@gmail.com>
2018-06-04 22:56:13 +0200

    as
    as

---
+++ www/php/select_1.php
@@ -0,0 +1,28 @@
+<?php
+	header("Access-Control-Allow-Origin: *");
+	$servername = "mysql.hostinger.es";
+	$username = "u261058157_admin";
+	$password = "mImUVY2g";
+	$dbname = "u261058157_ma";
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "(SELECT * FROM denuncia ORDER BY codi DESC LIMIT 1) ORDER BY codi ASC ";
+	$result = $conn->query($sql);
+	$rows = array();
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$rows[] = $row;
+		}
+	} else {
+		echo "0 results";
+	}
+	print json_encode($rows);
+	$conn->close();
+?>
