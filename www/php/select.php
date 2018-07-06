9da3957b76a39bb0fc4f6450a67546faa29895c0
jbertranr <jbertranr@gmail.com>
2018-07-07 01:50:47 +0200

    we
    we

--- www/php/select.php
+++ www/php/select.php
@@ -1,28 +1,50 @@
 <?php
-	header("Access-Control-Allow-Origin: *");
-	$servername = "mysql.hostinger.es";
-	$username = "u261058157_admin";
-	$password = "mImUVY2g";
-	$dbname = "u261058157_ma";
+    $codi = $_POST['codi'];
+    echo (codi);
+    
+    if (isset($_SERVER['HTTP_ORIGIN'])) {  
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");  
+        header('Access-Control-Allow-Credentials: true');  
+        header('Access-Control-Max-Age: 86400');   
+    }  
+      
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {  
+      
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))  
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");  
+      
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))  
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");  
+    }  
+   
+include "../../../..//access.php";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
 
-	$sql = "(SELECT * FROM denuncia ORDER BY codi DESC LIMIT 10) ORDER BY codi ASC ";
-	$result = $conn->query($sql);
-	$rows = array();
-	if ($result->num_rows > 0) {
-		// output data of each row
-		while($row = $result->fetch_assoc()) {
-			$rows[] = $row;
-		}
-	} else {
-		echo "0 results";
-	}
-	print json_encode($rows);
-	$conn->close();
+// Create connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (is_null($codi))
+{
+    $codi = 99999;
+}
+
+//$sql = "(SELECT * FROM denuncia WHERE projecte = 'depana' ORDER BY codi DESC LIMIT 10) ORDER BY codi DESC ";
+$sql = "(SELECT * FROM denuncia WHERE projecte = 'depana' AND codi < $codi ORDER BY codi DESC LIMIT 10)  ";
+$result = $conn->query($sql);
+$rows = array();
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+print json_encode($rows);
+$conn->close();
 ?>
